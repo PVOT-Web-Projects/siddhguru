2af802930751b81f45e44a5d19fb8a24fb24da6f
utkarshyadav11 <utkarshyadav6745@gmail.com>
2023-12-29 19:18:19 +0530

    npm x

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,14 @@ import HomePage3D from "../../components/3DHomeSection/HomePage3D";
 import EventsHomePage from "../../components/EventsHomePage/EventsHomePage";
 import FormHomePage from "../../components/Form_homePage/FormHomePage";
 import AboutAshram from "../../components/aboutAshram/AboutAshram";
-// import BrahmSutra from "../../components/brahmSutra/BramhSutra";
+import AllVideoHome from "../../components/allVideoHome/AllVideoHome";
+import BrahmSutra from "../../components/brahmSutra/BramhSutra";
 import Space from "../../components/counter_HomePage/CounterHomePage";
 import HeroSection from "../../components/heroSection/HeroSection";
 import HomePageSlider1 from "../../components/homePageSlider1/HomePageSlider1";
 import HomePageVideoSection from "../../components/homePageVideoSection/HomePageVideoSection";
+// import OurProgramSlider from "../../components/ourProgramSlider/OurProgramSlider";
 import TextSection from "../../components/textSection/TextSection";
-
 const Home = () => {
   return (
     <div>
@@ -17,9 +18,11 @@ const Home = () => {
       <HomePageSlider1 />
       <AboutAshram />
       <HomePage3D />
-      {/* <BrahmSutra /> */}
+      <BrahmSutra />
       <EventsHomePage />
       <HomePageVideoSection />
+      {/* <OurProgramSlider /> */}
+      <AllVideoHome />
       <FormHomePage />
       <Space />
     </div>
