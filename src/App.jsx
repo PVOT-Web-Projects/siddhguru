8c52db3465a7e0323af560cc4556ad0e5af30530
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-01-01 19:27:05 +0530

    npx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
+import { useState , useEffect} from "react";
 import { Route, Routes } from "react-router-dom";
+import LocomotoveScroll from "locomotive-scroll";
 import "./global.scss";
+import { AnimatePresence } from "framer-motion";
+import Loader from "./components/loader/Loader";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/home/Home";
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  setTimeout(() => {
+    setIsLoading(false);
+    document.body.style.cursor = "default";
+    window.scrollTo(0, 0);
+  }, 2000);
+
+  useEffect(() => {
+    const scroll = new LocomotoveScroll({
+      smooth: true,
+    });
+  });
+
+
+
+
+
+
+
+
+
   return (
-    <Layout>
+   <>
+<AnimatePresence mode="wait">{isLoading && <Loader />}</AnimatePresence>
+<Layout>
       <Routes>
         <Route path="/siddhguru1" element={<Home />} />
         <Route />
       </Routes>
     </Layout>
+   </>
   );
 }
 
--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -1,29 +1,69 @@
 import "./navbar.scss";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 const Navbar = () => {
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Only trigger once
+  });
   return (
     <div className="navbar">
       <ul>
-        <li>
+        <motion.li
+          ref={ref}
+          initial={{ opacity: 0, y: 0 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6, delay: 2.2 }}
+        >
           <a href="#">Siddhguru</a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          ref={ref}
+          initial={{ opacity: 0, y: 0 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6, delay: 2.3 }}
+        >
           <a href="#">About Ashram</a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          ref={ref}
+          initial={{ opacity: 0, y: 0 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6, delay: 2.4 }}
+        >
           <a href="#">Knowledge</a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          ref={ref}
+          initial={{ opacity: 0, y: 0 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6, delay: 2.5 }}
+        >
           <a href="#">Activities</a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          ref={ref}
+          initial={{ opacity: 0, y: 0 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6, delay: 2.6 }}
+        >
           <a href="#">Events</a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          ref={ref}
+          initial={{ opacity: 0, y: 0 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6, delay: 2.7 }}
+        >
           <a href="#">Centers</a>
-        </li>
-        <li>
+        </motion.li>
+        <motion.li
+          ref={ref}
+          initial={{ opacity: 0, y: 0 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.6, delay: 2.8 }}
+        >
           <a href="#">Contact Us</a>
-        </li>
+        </motion.li>
       </ul>
     </div>
   );
