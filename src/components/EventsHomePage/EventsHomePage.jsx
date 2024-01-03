6d9a6d7beae8f5d8143ae2a476b4152c79c9b4a9
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-01-03 18:10:45 +0530

    fix8

--- src/components/EventsHomePage/EventsHomePage.jsx
+++ src/components/EventsHomePage/EventsHomePage.jsx
@@ -64,7 +64,7 @@ const EventsHomePage = () => {
           ref={ref}
           initial={{ opacity: 0, y: 50 }}
           animate={inView ? { opacity: 1, y: 0 } : {}}
-          transition={{ duration: 0.6, delay: 0.3 }}
+          transition={{ duration: 0.6, delay: 0.1 }}
           className="mobile_heading"
         >
           Events
