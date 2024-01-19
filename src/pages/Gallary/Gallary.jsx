48552f377cad691e08e013c2ce0537d133ca01f5
patelvishwa224 <123302508+patelvishwa224@users.noreply.github.com>
2024-01-19 13:34:05 +0530

    gallarybaner

--- src/pages/Gallary/Gallary.jsx
+++ src/pages/Gallary/Gallary.jsx
@@ -1,7 +1,11 @@
 import Galaryimages from "../../components/Gallaryimages/Galaryimage";
+import Galarybanner from "../../components/gallarybaner/Gallarybaner";
 const Gallery=()=>{
     return(
+        <>
+        <Galarybanner />
         <Galaryimages />
-    )
+        </>
+    );
 }
 export default Gallery;
