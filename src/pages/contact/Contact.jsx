70432994d83c98749dd9f98342425eadf2d6d4c0
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-01-20 16:32:52 +0530

    spinner changes

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,5 @@
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactAddress from "../../components/contactAddress/ContactAddress";
-import FormCommon from "../../components/formCommon/FormCommon";
 import InnerPageCommonHeading from "../../components/innerPageCommonHeading/InnerPageCommonHeading";
 import Map from "../../map/Map";
 import "./contact.scss";
@@ -17,11 +16,9 @@ const Contact = () => {
             industry.
           </div>
           <div className="content">
-            Lorem Ipsum has been the industry's standard dummy text ever since
-            the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
-            five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged
+            {
+              " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged"
+            }
           </div>
         </div>
         <div className="contact_wrapper_right">
