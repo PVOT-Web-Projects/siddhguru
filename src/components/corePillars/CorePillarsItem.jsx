c6bdd08bd9bcf60845e558848a2968d927fdfec4
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-01-26 18:46:51 +0530

    core pillar animation -m

--- src/components/corePillars/CorePillarsItem.jsx
+++ src/components/corePillars/CorePillarsItem.jsx
@@ -1,13 +1,24 @@
 import PropTypes from "prop-types";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-const CorePillarsItem = ({ image, title, id }) => {
+const CorePillarsItem = ({ image, title, id, delayTime }) => {
+  const [ref, inView] = useInView({
+    triggerOnce: true, // Only trigger once
+  });
   return (
-    <div className={`corePillarsItem image_${id}`}>
+    <motion.div
+      className={`corePillarsItem image_${id}`}
+      ref={ref}
+      initial={{ opacity: 0, y: 0, x:-100 }}
+      animate={inView ? { opacity: 1, x: 0 } : {}}
+      transition={{ duration: 0.6, delay: delayTime }}
+    >
       <div className={`corePillarsItem_image`}>
-        <img src={image} alt={title}  />
+        <img src={image} alt={title} />
         <div className="title">{title}</div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 export default CorePillarsItem;
@@ -15,4 +26,5 @@ CorePillarsItem.propTypes = {
   id: PropTypes.number,
   image: PropTypes.any,
   title: PropTypes.string,
+  delayTime: PropTypes.number,
 };
