3da3fd0abf6af3e217a5b727e2d0b2d939710463
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-01-31 19:11:30 +0530

    new page added and nav changes -m

---
+++ src/pages/experience/Experience.jsx
@@ -0,0 +1,12 @@
+import InnerPageCommonHeading from "../../components/innerPageCommonHeading/InnerPageCommonHeading";
+import VideoCards from "../../components/videoCards/VideoCards";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <InnerPageCommonHeading heading={"Experience"} />
+      <VideoCards />
+    </div>
+  );
+};
+export default Experience;
