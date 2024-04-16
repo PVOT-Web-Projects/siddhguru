521505eb5cbc8c780e43399e07d726baeb2b1f2e
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-04-16 16:57:29 +0530

    ssa

--- src/components/header/logo/Logo.jsx
+++ src/components/header/logo/Logo.jsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../images/newLogo.png";
+import newLogo from "../../../images/newLogo.png";
+import hindiLogo from "../../../images/newLogoHindi.png";
 import "./logo.scss";
-
+import LanguageContext from "../../../components/language/Language";
 const Logo = () => {
+  const { language } = useContext(LanguageContext); 
+  const deskImg = language === "en" ? newLogo : hindiLogo;
+
   return (
     <div className="logo">
       <Link to={"/"}>
-      <img src={logo} alt="logo" />
+      <img src={deskImg} alt="logo" />
       </Link>
     </div>
   );
