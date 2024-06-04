5d113d6f4aa344ed8ea4a923f9c7e75a51fd023f
meetchauhandweck <meet.chauhan@dweckenterprise.com>
2024-06-04 13:10:10 +0530

    GA code -m

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import "./fonts/Lohit-Devanagari.ttf";
 import { LanguageProvider } from "./components/language/Language.jsx";
 
